d3adc961473a9a95796583c367515201ae731984
unknown <collinzizhen@gmail.com>
2020-10-27 19:44:22 -0700

    Updates

---
+++ precache-manifest.85a8eda144ca00becbf4a0643af20edb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1da6bef6c2f894a942a7dbd38d97ca",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "83a42e2ed61f1167bf94",
+    "url": "/react-deploy/static/css/2.00a338ba.chunk.css"
+  },
+  {
+    "revision": "0f080b1b853647643928",
+    "url": "/react-deploy/static/css/main.c5eb4ff9.chunk.css"
+  },
+  {
+    "revision": "83a42e2ed61f1167bf94",
+    "url": "/react-deploy/static/js/2.baf46e8a.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/react-deploy/static/js/2.baf46e8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f080b1b853647643928",
+    "url": "/react-deploy/static/js/main.22877e67.chunk.js"
+  },
+  {
+    "revision": "279d93b534695ff30aaf",
+    "url": "/react-deploy/static/js/runtime-main.5efb0f60.js"
+  }
+]);
